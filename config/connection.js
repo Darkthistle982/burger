dc63b7c4423332e0f745d8ef8e854f5a1236cad3
Brian Allen <brianallen2401@gmail.com>
2020-03-31 12:44:24 -0600

    added JAWSDB logic to connection for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 const mysql = require("mysql");
+let connection;
 
-let connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+  });
+}
 
 connection.connect(function(error) {
   if (error) {
@@ -15,6 +20,5 @@ connection.connect(function(error) {
   }
   console.log("Connected as id: " + connection.threadId);
 });
-
 
 module.exports = connection;
